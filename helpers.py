61f385ab6387336b08d28e6d8592038b2460ae4a
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-08-05 11:36:56 +0500

    refactor: Improve extraction of name and CNIC in extract_name_and_cnic function

--- helpers.py
+++ helpers.py
@@ -1,8 +1,6 @@
 import json
 import os
 import re
-from typing import Counter
-from paddleocr import PaddleOCR, draw_ocr
 import cv2
 import numpy as np
 from helpers import *
@@ -24,81 +22,8 @@ def crop_frame(frame, start_x, start_y, width, height):
     """
     return frame[start_y:start_y + height, start_x:start_x + width]
 
-def do_OCR_on_cropped_frame(ocr, frame):
-
-    result = ocr.ocr(frame, cls=True)
-
-    if len(result) == 0:
-        return None
-
-    for idx in range(len(result)):
-        res = result[idx]
-    return res
-
-def is_upside_down(ocr, image):
-    """
-    Determine if the given image has text that is upside down by comparing confidence scores.
-    
-    Args:
-    - ocr: The PaddleOCR instance.
-    - image: The input image.
-    
-    Returns:
-    - bool: True if the image is upside down, False otherwise.
-    """
-    # Perform OCR on the original image
-    result_original = ocr.ocr(image, cls=True)
-    if not result_original or not result_original[0]:
-        return False
-
-    original_confidence = np.mean([line[1][1] for line in result_original[0]])
-
-    # Flip the image upside down
-    flipped_image = cv2.flip(image, -1)
-
-    # Perform OCR on the flipped image
-    result_flipped = ocr.ocr(flipped_image, cls=True)
-    if not result_flipped or not result_flipped[0]:
-        return False
-
-    flipped_confidence = np.mean([line[1][1] for line in result_flipped[0]])
-
-    # Compare confidence scores
-    return flipped_confidence > original_confidence
-
-def correct_orientation(frame, should_flip):
-    """
-    Corrects the orientation of the given frame using PaddleOCR.
-    
-    Args:
-    - frame: The input image frame.
-    
-    Returns:
-    - The correctly oriented frame.
-    """
-    if should_flip:
-        return cv2.flip(frame, -1)
-    return frame
-
-def most_common_name_and_cnic(data):
-    # Separate names and CNICs into their own lists
-    names = [name for name, cnic in data]
-    cnics = [cnic for name, cnic in data]
-    
-    # Use Counter to count occurrences
-    name_counter = Counter(names)
-    cnic_counter = Counter(cnics)
-    
-    # Find the most common name and CNIC
-    most_common_name = name_counter.most_common(1)
-    most_common_cnic = cnic_counter.most_common(1)
-    
-    # Return the most common name and CNIC
-    return most_common_name[0] if most_common_name else (None, 0), most_common_cnic[0] if most_common_cnic else (None, 0)
-
 def get_cropped_frame(image):
-    
-    image = image[300:950, 400:1400]
+
     ROI = image
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -122,30 +47,6 @@ def get_cropped_frame(image):
             ROI = image[y:y+h, x:x+w]
 
     return ROI
-
-def check_if_majority_of_frame_is_white(image):
-    # check if majority of frame is white
-    if np.mean(image) > 170:
-        return True
-    else:
-        return False
-    
-def parse_data(data):
-    # Extract the text from the data
-    text = [entry[1][0] for entry in data]
-
-    return text
-
-def extract_all_details_str(data):
-    # Extract all details from the data
-    details = ""
-    for i, text in enumerate(data):
-        if text is None:
-            continue
-        details += text + " "
-    
-    return details
-
 
 def extract_name_and_cnic(data):
 
@@ -185,34 +86,6 @@ def extract_name_and_cnic(data):
     
     return name, n_confidence, cnic, c_confidence
 
-def is_majority_whitish(image, threshold=0.7):
-    
-    # Convert the image to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Define a threshold to consider a pixel as white
-    white_threshold = 170  # This can be adjusted based on your needs
-    
-    # Threshold the grayscale image to create a binary image
-    _, binary_image = cv2.threshold(gray_image, white_threshold, 255, cv2.THRESH_BINARY)
-    
-    # Count the number of white pixels
-    white_pixel_count = np.sum(binary_image == 255)
-    
-    # Calculate the total number of pixels
-    total_pixels = binary_image.size
-    
-    # Calculate the proportion of white pixels
-    white_pixel_ratio = white_pixel_count / total_pixels
-
-    # print(white_pixel_ratio)
-    
-    # Check if the majority of the image is whitish
-    is_whitish = white_pixel_ratio >= threshold
-    
-    return is_whitish
-
-
 def create_data_json(filename='data.json'):
     """Creates a data.json file with an empty list if it doesn't already exist."""
     if not os.path.exists(filename):
@@ -221,7 +94,6 @@ def create_data_json(filename='data.json'):
         print(f"{filename} created.")
     else:
         print(f"{filename} already exists.")
-
 
 def append_to_data_json(info, filename='data.json'):
     """Appends a stringified JSON object to the data.json file."""
@@ -245,92 +117,12 @@ def save_cnic_image(image, filename='image.jpg'):
     cv2.imwrite(f"{config('CNIC_SAVE_PATH')}/{filename}", image)
     print(f"Image saved as {filename}.")
 
-import psycopg2
-
 def extract_card_details(data):
     card_details = []
     for item in data:
-        # Extract the text detail from the second element of each item
-        detail = item[1][0]
+        detail = item[0]
         card_details.append(detail)
     return card_details
-
-# adds data to postgreSQL database
-def add_data_to_database(name_and_cnic, all_info):
-    while True:
-        try:
-            conn = psycopg2.connect(
-                dbname=config('DB_NAME'),
-                user=config('DB_USER'),
-                password=config('DB_PASSWORD'),
-                host=config('DB_HOST'),
-                port=config('DB_PORT')
-            )
-            break
-        except psycopg2.OperationalError:
-            print("Error connecting to PostgreSQL. Retrying...")
-            continue
-
-    cur = conn.cursor()
-
-    cnic = name_and_cnic[1][0]
-    name = "Unknown" if name_and_cnic[0][0] is None else name_and_cnic[0][0]
-    n_confidence = name_and_cnic[0][1]/5 if name != "Unknown" else 0
-
-    if name_and_cnic[1][1] < 0.8:
-        print(f"CNIC Confidence level too low: {name_and_cnic[1][1]}")
-        return
-    
-    if cnic is None:
-        print("CNIC not found")
-        return
-
-    check_if_cnic_exists_query = "SELECT * FROM cnic WHERE cnic = %s"
-
-    cur.execute(check_if_cnic_exists_query, (cnic,))
-    result = cur.fetchall()
-
-    if len(result) > 0:
-        print(f"Cnic already exists in database: {cnic}")
-
-        result = result[0]
-        
-        name_confidence_database = result[2]
-
-        if n_confidence > name_confidence_database:
-            print("confidence level is higher than the one in database")
-            print(f"Updating name for cnic: {cnic}")
-            update_name_query = "UPDATE cnic SET name = %s, name_confidence = %s, all_details = %s WHERE cnic = %s"
-
-            cur.execute(update_name_query, (name, n_confidence, extract_card_details(all_info), cnic))
-            conn.commit()
-
-        # return
-    else:
-        print(f"Cnic does not exist in database: {cnic}")
-        cnic_img_path = f'cnics/{cnic}.jpg'
-
-
-        print(extract_card_details(all_info))
-
-        insert_query = "INSERT INTO cnic (cnic, name, name_confidence, all_details, cnic_img_path) VALUES (%s, %s, %s, %s, %s)"
-
-        cur.execute(insert_query, (cnic, name, n_confidence, extract_card_details(all_info), cnic_img_path))
-        conn.commit()
-
-
-
-    add_timstamp_query = "INSERT INTO timestamp (cnic, timestamp) VALUES (%s, now())"
-
-    cur.execute(add_timstamp_query, (cnic,))
-
-    conn.commit()
-
-    cur.close()
-
-    conn.close()
-
-    print(f"Data added to database: {name_and_cnic}")
 
 def get_center_frame(frame):
     # get 50x50 pixel from the center of the frame
@@ -359,3 +151,16 @@ def process_batch_ocr_results(ocr_results):
     giberish = ocr_results[2]
 
     return boxes, texts, giberish
+
+def get_mp_list_object_from_cam_id(cam_id, mp_list):
+    for item in mp_list:
+        if item["cam_id"] == cam_id:
+            return item
+    return None
+
+def update_mp_list_object_from_cam_id(cam_id, mp_list, key, new_data):
+    for item in mp_list:
+        if item["cam_id"] == cam_id:
+            item[key] = new_data
+            return
+    return None
