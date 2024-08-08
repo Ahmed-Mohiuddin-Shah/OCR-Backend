79670a34b5bbb9a00b9015b8a3c0d58c73fa57ce
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-08-08 17:00:34 +0500

    refactor: Update main.py to use multiprocessing Manager for shared variables
    The code changes in main.py update the usage of shared variables by implementing the multiprocessing Manager. This change improves the efficiency and reliability of the code by ensuring proper synchronization and communication between processes.

--- main.py
+++ main.py
@@ -6,47 +6,28 @@ from database_operations import get_db_config
 
 async def main():
 
-    frame_queue = mp.Queue()
-    ocr_results_queue = mp.Queue()
-    previously_saved_cnic = mp.Manager().list(
-        [
-            {
-                "cam_id": -1,
-                "cnic": None,
-            }
-        ]
-    )
     
-    card_already_in_holder = mp.Manager().list(
-        [
-            {
-                "cam_id": -1,
-                "status": False
-            }
-        ]
-    )
-
-    number_plate_detect_cache = mp.Manager().list(
-        [
-            {
-                "cam_id": -1,
-                "number_plate": []
-            }
-        ]
-    )
 
     db_config = await get_db_config()
 
     cameras = db_config["cameras"]
 
-    run_system(
+    frame_queue = mp.Queue()
+    ocr_results_queue = mp.Queue()
+
+    with mp.Manager() as manager:
+        previously_saved_cnic = manager.list()
+        card_already_in_holder = manager.list()
+        number_plate_detect_cache = manager.list()
+
+        run_system(
         cams=cameras,
         frame_queue=frame_queue, 
         ocr_results_queue=ocr_results_queue,
         previously_saved_cnic=previously_saved_cnic, 
         card_already_in_holder=card_already_in_holder,
         number_plate_detect_cache=number_plate_detect_cache
-    )
+        )
 
 if __name__ == "__main__":
     mp.set_start_method("spawn")
