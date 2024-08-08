d1a193388d1fcc72b03644071980f609d66e110e
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-08-08 16:50:18 +0500

    ```text refactor: Add new entry to current_cache_number_plates in number_plate.py
    This commit adds a new entry to the 'current_cache_number_plates' list in the 'number_plate.py' file. The new entry consists of the 'number_plates', 'timestamp', and 'frame' variables. This change enhances the functionality of the code by updating the cache with the latest number plates detected.

--- patterns/number_plate.py
+++ patterns/number_plate.py
@@ -77,8 +77,9 @@ def post_detection_pattern_for_num_plate_rfid(
     )["number_plates"]
 
     if len(number_plates) > 0:
+        new_entry = (number_plates, timestamp, frame)
         current_cache_number_plates.append(
-            [number_plates, timestamp, frame]
+            new_entry
         )
         print(len(current_cache_number_plates), cam_id, current_cache_number_plates)
         update_mp_list_object_from_cam_id(
