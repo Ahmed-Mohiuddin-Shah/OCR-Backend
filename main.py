86fddf11aeb7bc7dbbcac73e1e8e06ec8788efee
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-09-05 14:48:07 +0500

    chore: docs test using pdoc3

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import multiprocessing as mp
 import asyncio
+from typing import List
 
 from patterns.patterns import run_system
 from database_operations import get_db_config
 
-async def main(manager=None):
+
+async def main(manager: List = None) -> None:
+    """
+    Main function to run the system
+    
+    Be sure to run this function in an async function
+    
+    Be sure to set the start method of multiprocessing to "spawn" before calling this function
+    """
 
     db_config = await get_db_config()
 
@@ -19,12 +28,13 @@ async def main(manager=None):
 
     run_system(
         cams=cameras,
-        frame_queue=frame_queue, 
+        frame_queue=frame_queue,
         ocr_results_queue=ocr_results_queue,
-        previously_saved_cnic=previously_saved_cnic, 
+        previously_saved_cnic=previously_saved_cnic,
         card_already_in_holder=card_already_in_holder,
-        number_plate_detect_cache=number_plate_detect_cache
-        )
+        number_plate_detect_cache=number_plate_detect_cache,
+    )
+
 
 if __name__ == "__main__":
     mp.set_start_method("spawn")
